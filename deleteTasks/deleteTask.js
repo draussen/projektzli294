f578bd206ff2a499b60fd1bb9d28598a6a283203
draussen <drinsaiti@gmail.com>
2022-09-17 19:16:42 +0200

    tasks can now be deleted

--- deleteTasks/deleteTask.js
+++ deleteTasks/deleteTask.js
@@ -13,13 +13,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function deleteTask() {
 
-var data = [fetch('http://127.0.0.1:3000/tasks')
-.then(function(response){
-    return response.json();
-})];
+    const id = document.getElementById("deleteTaskInput").value;
 
-var deleteTaskInput = document.getElementById("deleteTaskInput").value;
-var index = data.findIndex(obj => obj.id == deleteTaskInput);
-
-console.log(index);
+    let response = fetch('http://127.0.0.1:3000/task/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        mode: 'cors'
+    })
+        .then((response) => {
+            if (response.status == 400) {
+                console.log(response)
+            }
+            return response;
+        })
 }
