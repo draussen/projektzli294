740b97632500550fb521d7d030edb40948615154
draussen <drinsaiti@gmail.com>
2022-10-03 16:24:07 +0200

    created the html file with css for styling and js for the functions

--- deleteTasksAuth/deleteTask.js
+++ deleteTasksAuth/deleteTask.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    if (localStorage.getItem("token") == undefined) {
+    if (localStorage.getItem("token") == "undefined") {
         window.location.href = "http://127.0.0.1:3001/authentication/authentication.html"
 
     }
+
+    const deleteTaskForm = document.getElementById("deleteTaskForm")
 
     deleteTaskForm.addEventListener("submit", (event) => {
         event.preventDefault();
@@ -18,7 +20,7 @@ function deleteTask() {
 
     const id = document.getElementById("deleteTaskInput").value;
 
-    let response = fetch('http://127.0.0.1:3000/auth/jwt/task/' + id, {
+    fetch('http://127.0.0.1:3000/auth/jwt/task/' + id, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
@@ -32,4 +34,27 @@ function deleteTask() {
             }
             return response;
         })
+        .then((data) => {
+            if (data.status == 404) {
+                document.getElementById("message").style.display = "block";
+                document.getElementById("message").style.color = "red";
+                document.getElementById("message").textContent = "ID wasn't found. Please enter a valid ID.";
+
+                setTimeout(hideElement, 3000);
+            }
+            else {
+                document.getElementById("message").style.display = "block";
+                document.getElementById("message").style.color = "green";
+                document.getElementById("message").textContent = "Task was successfully deleted!";
+
+                setTimeout(hideElement, 3000);
+
+            }
+        })
+
+
+}
+
+function hideElement() {
+    document.getElementById("message").style.display = "none";
 }
