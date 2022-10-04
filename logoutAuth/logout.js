994dcb816dc398c8f646626175cd7c4bdee60a4d
draussen <drinsaiti@gmail.com>
2022-10-04 16:32:43 +0200

    created logout js added html incase it breaks

---
+++ logoutAuth/logout.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    localStorage.setItem("token", undefined)
+    window.location.href = "http://127.0.0.1:3001/authentication/authentication.html"
+}) 
